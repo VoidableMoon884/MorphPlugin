769f7e4d7c82001c321af4a2641095ceacc3c3af
VoidableMoon884 <trash_02@nogacity.de>
2024-03-28 11:15:34 +0100

    Created Projekt

---
+++ MorphPlugin/src/main/java/de/vmoon/morphplugin/MorphPlugin.java
@@ -0,0 +1,17 @@
+package de.vmoon.morphplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MorphPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
