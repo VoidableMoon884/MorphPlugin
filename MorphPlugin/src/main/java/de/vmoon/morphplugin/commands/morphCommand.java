08eaf1ae4fc75385b0ab05044deca431670e8904
VoidableMoon884 <trash_02@nogacity.de>
2024-03-28 11:38:08 +0100

    Changes for 1.0.1 TabCompleter

--- MorphPlugin/src/main/java/de/vmoon/morphplugin/commands/morphCommand.java
+++ MorphPlugin/src/main/java/de/vmoon/morphplugin/commands/morphCommand.java
@@ -3,10 +3,15 @@ package de.vmoon.morphplugin.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
-public class morphCommand implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class morphCommand implements CommandExecutor, TabCompleter {
+    private EntityType currentEntity = null;
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (!(commandSender instanceof Player)) {
@@ -14,23 +19,39 @@ public class morphCommand implements CommandExecutor {
             return true;
         }
 
+        Player player = (Player) commandSender;
+
         if (strings.length < 1) {
             commandSender.sendMessage("Bitte gib eine Entität an, in die du dich verwandeln möchtest.");
             return false;
         }
 
-        Player player = (Player) commandSender;
-        String entityName = strings[0].toLowerCase(); // Entität in Kleinbuchstaben konvertieren
+        String entityName = strings[0].toUpperCase();
 
         try {
-            EntityType entityType = EntityType.valueOf(entityName.toUpperCase());
-            player.getWorld().spawnEntity(player.getLocation(), entityType);
+            EntityType entityType = EntityType.valueOf(entityName);
+            this.currentEntity = entityType;
             player.sendMessage("Du hast dich in " + entityType.name() + " verwandelt.");
         } catch (IllegalArgumentException e) {
-            commandSender.sendMessage("Ungültige Entität angegeben.");
+            player.sendMessage("Ungültige Entität angegeben.");
             return false;
         }
 
         return true;
     }
+
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> completions = new ArrayList<>();
+        if (args.length == 1) {
+            String query = args[0].toUpperCase();
+            for (EntityType entityType : EntityType.values()) {
+                if (entityType.isAlive() && entityType.name().startsWith(query)) {
+                    completions.add(entityType.name());
+                }
+            }
+        }
+        return completions;
+    }
+
+
 }
